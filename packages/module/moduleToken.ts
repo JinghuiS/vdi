9897565d245120459bd0c7630ec4e1dddb9a09b1
石京辉 <1292077619@qq.com>
2022-05-16 13:40:55 +0800

    feat: 🎸 module
    add VDI_CONFIG

--- packages/module/moduleToken.ts
+++ packages/module/moduleToken.ts
@@ -1,6 +1,6 @@
 import { createIdentifier, Injector } from '@wendellhu/redi'
 import { App, InjectionKey } from 'vue'
-
+import { VdiConfig } from './config'
 export const VUE_APP = createIdentifier<App>('VUE_APP')
 
 export type APP_INITIALIZER_TYPE = {
@@ -12,3 +12,5 @@ export const APP_INITIALIZER = createIdentifier<APP_INITIALIZER_TYPE>(
 
 export const VUE_INJECTOR_KEY: InjectionKey<Injector> =
     Symbol('VUE_INJECTOR_KEY')
+
+export const VDI_CONFIG = createIdentifier<typeof VdiConfig>('VDI_CONFIG')
