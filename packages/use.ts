8ae5df33ca8c30524f87de4f6cf427ab498f4a3d
石京辉 <1292077619@qq.com>
2023-02-02 21:01:39 +0800

    feat: 🎸 Vdi plugin add injector

--- packages/use.ts
+++ packages/use.ts
@@ -4,7 +4,13 @@ import { VUE_APP, VUE_INJECTOR_KEY } from './hooks/token'
 
 function vdi(providers?: Dependency[]): Plugin {
     return {
-        install(app, ...options) {
+        install(app, RootInjector?: Injector, ...options) {
+            if (RootInjector) {
+                RootInjector.add([VUE_APP, { useValue: app }])
+                const childInjector = RootInjector.createChild(providers)
+                app.provide(VUE_INJECTOR_KEY, childInjector)
+                return
+            }
             let _providers: Dependency[] = [[VUE_APP, { useValue: app }]]
             if (providers) {
                 _providers = [..._providers, ...providers]
